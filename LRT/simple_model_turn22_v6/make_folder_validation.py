9bb305a21d81c3597e40cbdef8043525b83392e6
Schwalbe262 <r1jae262@uos.ac.kr>
2023-01-25 16:19:14 +0900

    LRT#12 2/2턴 모델_v6 validation

---
+++ LRT/simple_model_turn22_v6/make_folder_validation.py
@@ -0,0 +1,21 @@
+import yaml
+import os
+import subprocess
+import random
+import csv
+import time
+import csv
+import numpy as np
+import math
+import pandas
+import os
+import shutil
+
+
+for k in range(2, 11): 
+
+
+    shutil.copytree("./script_validation1", f'./script_validation{k}')
+
+
+
